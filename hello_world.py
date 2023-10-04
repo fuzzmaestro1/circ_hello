f3cb541235f469b89aa9c9ecc1536958f8893a90
fuzzmaestro1 <122757141+fuzzmaestro1@users.noreply.github.com>
2023-10-04 15:12:06 -0700

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print("Welcome to Semgrep!" + "Use our Run button to start experimenting -->")
+
+
+print("Hello World")
+
+# To detect ALL calls to the print() function, change the Semgrep Rule from print("...") to print(...)
+
+print("Hello World")
+
+
+
+print("HELLO WORLD")
